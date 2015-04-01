de391acb502117b60f627af0cc4929f1c0fe9895
patrick0xf <patrick0xf@thunderground.net>
2015-03-31 18:22:57 -0700

    Revert "Revert "Initial Commit""
    This reverts commit 523eb4bdd936a37086372631c4c4772bf8eb3b48.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PostureApplication
+{
+    static class Program
+    {
+        static readonly NotifyIcon NotifyIcon = new NotifyIcon();
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.ApplicationExit += ApplicationOnApplicationExit;
+            Application.Run(new PostureApplicationContext(NotifyIcon));
+        }
+
+        private static void ApplicationOnApplicationExit(object sender, EventArgs eventArgs)
+        {
+            NotifyIcon.Visible = false;
+        }
+    }
+}
